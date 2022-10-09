62b1d6c968e44a9edd8830edd56f07b792cb60b3
hshima <22624182+hshima@users.noreply.github.com>
2022-10-09 00:44:14 -0300

    Remove cache par gitignore

--- src/main/java/br/vet/sidekick/poc/service/FuncionarioService.java
+++
@@ -1,9 +0,0 @@
-package br.vet.sidekick.poc.service;
-
-import br.vet.sidekick.poc.model.Funcionario;
-
-public interface FuncionarioService {
-    Boolean authenticate(String username, String password);
-
-    Funcionario get(Funcionario funcionario);
-}
