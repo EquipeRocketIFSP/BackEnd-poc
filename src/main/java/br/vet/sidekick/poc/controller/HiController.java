e8b7a0c7a8e7367be9f9e65c1f5f37bb57f0fd47
hshima <22624182+hshima@users.noreply.github.com>
2022-10-10 20:44:19 -0300

    Aplica ultimas alterações para a POC

---
+++ src/main/java/br/vet/sidekick/poc/controller/HiController.java
@@ -0,0 +1,19 @@
+package br.vet.sidekick.poc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+@CrossOrigin
+@Slf4j
+public class HiController {
+    @GetMapping
+    public ResponseEntity<String> response(){
+        String sucesso = "Requição com sucesso, parabéns!!";
+        log.info(sucesso);
+        return ResponseEntity.ok(sucesso);
+    }
+}
