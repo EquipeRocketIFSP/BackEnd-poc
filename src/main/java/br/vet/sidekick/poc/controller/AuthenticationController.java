998f7064ef3dfead192411ac77a17df97ef5dff6
hshima <22624182+hshima@users.noreply.github.com>
2022-11-06 19:58:59 -0300

    Pequenas correções

--- src/main/java/br/vet/sidekick/poc/controller/AuthenticationController.java
+++ src/main/java/br/vet/sidekick/poc/controller/AuthenticationController.java
@@ -53,21 +53,17 @@ public class AuthenticationController {
         }catch (AuthenticationException e){
             return ResponseEntity.badRequest().build();
         }
-        // retornar nome e crmv
         Optional<Veterinario> vet = veterinarioService.findByEmail(login.getEmail());
-        System.out.println("vet: " + vet.toString());
         return  ResponseEntity.ok(
                 vet.isPresent()
                 ? TokenDto.builder()
                         .token(token)
-                        .type("Bearer")
                         .nome(vet.get().getNome())
                         .crmv(vet.get().getRegistroCRMV())
                         .build()
                 :
                         TokenDto.builder()
                         .token(token)
-                        .type("Bearer")
                         .build()
         );
 
